f3dfd3a38d0853d7f74bf179b778d751bb8011bd
Julian Lucero <julianlucero2012@gmail.com>
2024-08-30 22:41:04 -0300

    feat: Customizing Neotree Behavior

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,7 +2,39 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<Leader>e", "<cmd>Neotree reveal_force_cwd right<cr>", { desc = "File explorer" })
-vim.keymap.set("n", "<Leader>E", "<cmd>Neotree filesystem toggle<cr>", { desc = "Toggle file explorer" })
-vim.keymap.set("n", "<Leader>be", "<cmd>Neotree buffers reveal left<cr>", { desc = "Buffer explorer" })
-vim.keymap.set("n", "<Leader>bE", "<cmd>Neotree buffers toggle<cr>", { desc = "Buffer explorer" })
+-- File explorer keymaps
+vim.keymap.set("n", "<Leader>e", function()
+  local manager = require("neo-tree.sources.manager")
+  local renderer = require("neo-tree.ui.renderer")
+
+  local state = manager.get_state("filesystem")
+  local window_exists = renderer.window_exists(state)
+  local curr_buff_name = vim.api.nvim_buf_get_name(0)
+  local is_filesystem_focused = string.match(curr_buff_name, "filesystem")
+
+  if window_exists and is_filesystem_focused then
+    vim.cmd("Neotree filesystem toggle")
+  else
+    vim.cmd("Neotree reveal_force_cwd right")
+  end
+end, { desc = "Open/Close: File explorer" })
+vim.keymap.set("n", "<Leader>E", "<cmd>Neotree filesystem toggle<cr>", { desc = "Toggle: File explorer" })
+
+-- Buffers explorer keymaps
+vim.keymap.set("n", "<Leader>be", function()
+  local manager = require("neo-tree.sources.manager")
+  local renderer = require("neo-tree.ui.renderer")
+
+  local state = manager.get_state("buffers")
+
+  local window_exists = renderer.window_exists(state)
+  local curr_buff_name = vim.api.nvim_buf_get_name(0)
+  local is_filesystem_focused = string.match(curr_buff_name, "buffers")
+
+  if window_exists and is_filesystem_focused then
+    vim.cmd("Neotree buffers toggle")
+  else
+    vim.cmd("Neotree buffers reveal left")
+  end
+end, { desc = "Open/Close: Buffers explorer" })
+vim.keymap.set("n", "<Leader>bE", "<cmd>Neotree buffers toggle<cr>", { desc = "Toggle: Buffer explorer" })
