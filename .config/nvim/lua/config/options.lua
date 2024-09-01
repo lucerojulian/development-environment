ff987bc877d2bcb527405460d3871fafac59f251
Julian Lucero <julianlucero2012@gmail.com>
2024-09-01 17:03:39 -0300

    refactor: Changed tabsize to 4 in neovim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,3 +3,18 @@
 -- Add any additional options here
 
 vim.g.zellij_navigator_no_default_mappings = 1
+vim.g.mapleader = " "
+
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+vim.opt.number = true
+
+vim.opt.title = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+vim.opt.smarttab = true
+vim.opt.breakindent = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
